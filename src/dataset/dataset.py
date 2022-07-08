1ea0029f797f8600fc9e3f77d97d98f9961c4710
yessense <akorchemnyj@gmail.com>
2022-07-08 09:54:21 +0300

    Added model parameters to argument parsing

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -135,10 +135,10 @@ class DatasetCreator():
                 experiment_data = experiment_data.to_numpy()
 
                 x = experiment_data[:, :-1]
-                x = sn.lfilter(self.b, self.a, x, axis=1)
-                x = sn.lfilter(self.b50, self.a50, x, axis=1)
-                x -= x.mean(axis=1)[:, np.newaxis]
-                x /= x.std(axis=1)[:, np.newaxis]
+                x = sn.lfilter(self.b, self.a, x, axis=0)
+                x = sn.lfilter(self.b50, self.a50, x, axis=0)
+                x -= x.mean(axis=0)[:, np.newaxis]
+                x /= x.std(axis=0)[:, np.newaxis]
                 x = roll2d(x, (self.dt, len(self.used_columns)), 1, shift).squeeze()
                 x = x.transpose(0, 2, 1)
 
