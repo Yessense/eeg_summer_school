113f01457cd2c15e50ac0e20d6b34fc31de01677
yessense <akorchemnyj@gmail.com>
2022-07-12 17:11:32 +0300

    Added train_test_split_max

--- src/model/im_classifier.py
+++ src/model/im_classifier.py
@@ -148,7 +148,7 @@ class IMClassifier(pl.LightningModule):
             self.log("Val Loss", loss)
             self.log("Val Accuracy", accuracy, prog_bar=True)
             person_loss = self.loss_func(person_predicted, person_target)
-            person_accuracy = self.accuracy(torch.argmax(im_predicted, dim=1), im_target)
+            person_accuracy = self.accuracy(torch.argmax(person_predicted, dim=1), person_target)
             self.log("Person Loss", person_loss)
             self.log("Person Accuracy", person_accuracy)
 
@@ -165,7 +165,7 @@ class IMClassifier(pl.LightningModule):
         im_accuracy = self.accuracy(torch.argmax(im_predicted, dim=1), im_target)
 
         person_loss = self.loss_func(person_predicted, person_target)
-        person_accuracy = self.accuracy(torch.argmax(im_predicted, dim=1), im_target)
+        person_accuracy = self.accuracy(torch.argmax(person_target, dim=1), person_predicted)
 
         self.log("Train Loss", im_loss)
         self.log("Person Loss", person_loss)
